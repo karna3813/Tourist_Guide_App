644e73ada7ca034e77314b3e2c9d78cc065af3de
Shivam Kumar <shivamkumarfcc440@gmail.com>
2023-08-16 17:04:06 +0530

    Updated App.js after completing the frontEnd.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,94 @@
-import React from 'react';
- import { BrowserRouter as Router , Route , Redirect, Switch } from 'react-router-dom';
+import React , { useState , useCallback } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
 import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlaces';
-import MainNavigation from './shared/Navigation/MainNavigation';
+import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
+
 const App = () => {
+
+  const [isLoggedIn , setIsLoggedin] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedin(true);
+  },[]);
+
+  const logout = useCallback(() => {
+    setIsLoggedin(false);
+  },[]);
+
+
+  let routes;
+
+  if(isLoggedIn){
+    routes = (
+      <React.Fragment>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+  else {
+    routes = (
+      <React.Fragment>
+        <Switch>
+          <Route path="/" exact>
+                <Users />
+              </Route>
+
+          <Route path="/:userId/places" exact>
+          <UserPlaces />
+          </Route>
+
+          <Route path="/auth" exact>
+            <Auth />
+          </Route>
+
+          <Redirect to="/auth" />
+          </Switch>
+      </React.Fragment>
+    );
+  }
   return (
-            
-      <Router>
-        <MainNavigation/>
-       <main>
-       <Switch>
-       <Route path = "/" exact>
-        <Users/>
-       </Route>
 
-       <Route path = "/:userId/places" exact>
-        <UserPlaces/>
-       </Route>
-       <Route path = "/places/new" exact>
-        <NewPlace/>
-       </Route>
-
-       <Route path = "/places/:placeId" exact>
-        <UpdatePlace/>
-       </Route>
-
-       <Redirect to ="/"/>
-       </Switch>
-       </main>
-      </Router> 
- );
+    <AuthContext.Provider value = {
+      {
+        isLoggedIn: isLoggedIn,
+        login: login,
+        logout: logout
+        }
+        }
+        >
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>{routes}</Switch>
+      </main>
+    </Router>
+    </AuthContext.Provider>
+  );
 };
 
 export default App;
